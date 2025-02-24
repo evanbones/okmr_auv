58071a2cec37d6e33d8f7d15ff182c4582ba6537
raphaelbart9 <raphaelbart9@gmail.com>
2025-02-23 21:14:13 -0800

    Build successful, test for funtionality.

--- ros2_ws/src/automated_planning/src/TurnCW.h
+++ ros2_ws/src/automated_planning/src/TurnCW.h
@@ -2,28 +2,33 @@
 #define TURNCW_H
 
 #include "MovementBaseClass.h"  
-#include <cascade_msgs/msg/movement_command.msg>  
-#include <geometry_msgs/msg/vector3>  
+#include <cascade_msgs/msg/movement_command.hpp>  
+#include <geometry_msgs/msg/pose_stamped.hpp>  
 
 class TurnCW : public MovementBaseClass {
 public:
     TurnCW(const std::string& name, const BT::NodeConfiguration& config);
 
-    static BT::PortsList providedPorts() {
-        return {BT::InputPort<std::string>("command")};
-    }
+//static BT::PortsList providedPorts() {
+   //     return {};
+   // }
+
+
+
+void setCommandData(cascade_msgs::msg::MovementCommand &msg) override;
 
     BT::NodeStatus tick() override;
 
-    void set_command_data(cascade_msgs::msg::MovementCommand &msg) override;
+  virtual ~TurnCW() = default;
+
 
 private:
  rclcpp::Node::SharedPtr node_;
-    rclcpp::Publisher<your_package::msg::MovementCommand>::SharedPtr publisher_;
-    rclcpp::Client<your_package::srv::CascadeStatus>::SharedPtr client_;
+    rclcpp::Publisher<cascade_msgs::msg::MovementCommand>::SharedPtr publisher_;
+    rclcpp::Client<cascade_msgs::srv::Status>::SharedPtr client_;
 
     bool message_sent_;
-    std::string command_;
+    int32_t command_;
 
 }; 
 
