f9953e2cb84a7c079b7c2a998e4f3cfd5c863b36
Jetson Orin Nano <jetson@okmarinerobotics.com>
2025-05-18 16:55:56 -0700

    added foxglove bridge to launch file

--- ros2_ws/launch/auv.launch.py
+++ ros2_ws/launch/auv.launch.py
@@ -2,6 +2,7 @@ from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.substitutions import PathJoinSubstitution
 from launch_ros.substitutions import FindPackageShare
+from launch_ros.actions import Node
 
 def generate_launch_description():
     #
