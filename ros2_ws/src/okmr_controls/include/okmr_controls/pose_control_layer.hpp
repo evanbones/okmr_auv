a77f45e1da648efd2da1e6146a46e934cf7edc64
Eryk <erykhalicki0@gmail.com>
2025-06-21 15:28:54 -0700

    L2 L3 controls updates

---
+++ ros2_ws/src/okmr_controls/include/okmr_controls/pose_control_layer.hpp
@@ -0,0 +1,31 @@
+#ifndef OKMR_CONTROLS_POSE_CONTROL_LAYER_HPP
+#define OKMR_CONTROLS_POSE_CONTROL_LAYER_HPP
+
+#include "okmr_controls/control_layer_base.hpp"
+#include <okmr_msgs/msg/relative_pose.hpp>
+#include <geometry_msgs/msg/twist.hpp>
+
+namespace okmr_controls
+{
+
+class PoseControlLayer : public ControlLayerBase
+{
+public:
+    PoseControlLayer();
+
+protected:
+    void update() override;
+
+private:
+    rclcpp::Subscription<okmr_msgs::msg::RelativePose>::SharedPtr pose_target_sub_;
+    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr velocity_target_pub_;
+    
+    okmr_msgs::msg::RelativePose current_pose_;
+    okmr_msgs::msg::RelativePose pose_target_;
+    
+    void pose_target_callback(const okmr_msgs::msg::RelativePose::SharedPtr msg);
+};
+
+}  // namespace okmr_controls
+
+#endif  // OKMR_CONTROLS_POSE_CONTROL_LAYER_HPP
