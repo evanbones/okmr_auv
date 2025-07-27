2ad78d33be09d45574f5990f6d2612d4d1bcd037
Eryk <erykhalicki0@gmail.com>
2025-07-26 19:59:45 -0700

    creating launch file for testing

--- ros2_ws/src/okmr_object_detection/launch/full_object_detection_system.launch.py
+++ ros2_ws/src/okmr_object_detection/launch/full_object_detection_system.launch.py
@@ -4,6 +4,7 @@ from launch import LaunchDescription
 from launch_ros.actions import Node
 from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
+from ament_index_python.packages import get_package_share_directory
 
 
 def generate_launch_description():
@@ -14,28 +15,20 @@ def generate_launch_description():
         description="Whether to use semantic depth subscriber (true) or normal depth subscriber (false)",
     )
     """
+    # add debug launch arg
+
+    pkg_share = get_package_share_directory("okmr_object_detection")
+    model_share_path = os.path.join(pkg_share, "models")
 
     bottom_depth_to_pointcloud_node = Node(
-        package="okmr_mapping",
-        executable="depth_to_pointcloud",
-        name="bottom_depth_to_pointcloud_node",
+        package="okmr_object_detection",
+        executable="onnx_segmentation_detector",
         output="screen",
-        parameters=[
-            {
-                "use_semantic_subscriber": LaunchConfiguration(
-                    "use_semantic_subscriber"
-                ),
-                "max_dist": 1.0,
-                "min_dist": 0.07,
-                "use_mask": False,
-                "use_rgb": False,
-            }
-        ],
+        parameters=[{"debug": True}, {"model_path": model_share_path}],
         remappings=[
-            ("/rgb", "/camera2/camera2/image_raw"),
-            ("/depth", "/camera2/camera2/image_depth"),
-            ("/camera_info", "/camera2/camera2/depth_camera_info"),
-            ("/pointcloud", "/camera2/pointcloud"),
+            ("/rgb", "/camera1/camera1/image_raw"),
+            ("/depth", "/camera1/camera1/image_depth"),
+            ("/labeled_image", "/labeled_image"),
         ],
     )
 
