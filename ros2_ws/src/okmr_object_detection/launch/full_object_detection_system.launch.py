0f973012d0b363675c80eaf9cbb492fc30de8c72
Eryk <erykhalicki0@gmail.com>
2025-07-26 18:15:25 -0700

    launch testing

---
+++ ros2_ws/src/okmr_object_detection/launch/full_object_detection_system.launch.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+
+
+def generate_launch_description():
+    """
+    use_semantic_arg = DeclareLaunchArgument(
+        "use_semantic_subscriber",
+        default_value="true",
+        description="Whether to use semantic depth subscriber (true) or normal depth subscriber (false)",
+    )
+    """
+
+    bottom_depth_to_pointcloud_node = Node(
+        package="okmr_mapping",
+        executable="depth_to_pointcloud",
+        name="bottom_depth_to_pointcloud_node",
+        output="screen",
+        parameters=[
+            {
+                "use_semantic_subscriber": LaunchConfiguration(
+                    "use_semantic_subscriber"
+                ),
+                "max_dist": 1.0,
+                "min_dist": 0.07,
+                "use_mask": False,
+                "use_rgb": False,
+            }
+        ],
+        remappings=[
+            ("/rgb", "/camera2/camera2/image_raw"),
+            ("/depth", "/camera2/camera2/image_depth"),
+            ("/camera_info", "/camera2/camera2/depth_camera_info"),
+            ("/pointcloud", "/camera2/pointcloud"),
+        ],
+    )
+
+    return LaunchDescription(
+        [
+            use_semantic_arg,
+            front_depth_to_pointcloud_node,
+            bottom_depth_to_pointcloud_node,
+        ]
+    )
