b8c4419f9769d5933084ef9a6590b3bcdf61635e
ErykHalicki <erykhalicki0@gmail.com>
2025-08-15 12:23:06 +0000

    locked

--- ros2_ws/src/okmr_hardware_interface/src/firmware/firmware.ino
+++ ros2_ws/src/okmr_hardware_interface/src/firmware/firmware.ino
@@ -6,7 +6,7 @@ float throttle[8] = {1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500};
 #define NUM_SERVOS 1
 #define NUM_ACTUATORS 3
 
-int pins[] = {12, 13, 14, 25, 26, 27, 32, 35}; //motor 9 is servo
+int pins[] = {12, 13, 14, 25, 26, 27, 32, 34}; 
 int servo_pins[] = {17};
 int servo_inits[] = {1500};
 int actuator_pins[] = {16, 4, 5};
