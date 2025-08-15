145a6b64e68b7735e4b24f0d8cb9ce50e3e3ccd4
Hasan <hasanbm.stu@gmail.com>
2025-08-14 20:32:57 -0700

    Sideays and rotating scan fixed :D

--- ros2_ws/src/okmr_automated_planner/okmr_automated_planner/state_machines/sideways_scan_state_machine.py
+++ ros2_ws/src/okmr_automated_planner/okmr_automated_planner/state_machines/sideways_scan_state_machine.py
@@ -1,6 +1,7 @@
 from okmr_automated_planner.base_state_machine import BaseStateMachine
+from okmr_automated_planner.state_machines.rotating_scan_state_machine import RotatingScanStateMachine
 from okmr_utils.logging import make_green_log
-from okmr_msgs.msg import MissionCommand, MovementCommand
+from okmr_msgs.msg import MissionCommand
 
 
 class SidewaysScanStateMachine(BaseStateMachine):
@@ -12,7 +13,7 @@ class SidewaysScanStateMachine(BaseStateMachine):
     ]
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.moving_sideways_distance = self.get_local_parameter("moving_sideways_distance")
+        self.scan_angle = self.get_local_parameter("moving_sideways_distance")
         
     def on_enter_initializing(self):
         self.queued_method = self.initialized
@@ -20,31 +21,56 @@ class SidewaysScanStateMachine(BaseStateMachine):
     def on_enter_moving_sideways(self):
         movement_msg = MovementCommand()
         movement_msg.command = MovementCommand.MOVE_RELATIVE
-        movement_msg.translation.x = self.moving_sideways_distance
+        movement_msg.translation.x = self.scan_angle
         
         success = self.movement_client.send_movement_command(
             movement_msg,
             on_success=self.moving_sideways_done,
             on_failure=self.abort,
         )
-        
+
         if not success:
             self.ros_node.get_logger().error("Failed to send sideways movement command")
             self.queued_method = self.abort
     
-    # need to figure out how to save initial position first
-    # def on_enter_returning_to_center(self):
-    #     movement_msg = MovementCommand()
-    #     movement_msg.command = MovementCommand.MOVE_RELATIVE
-    #    # movement_msg.translation.x = 
+    def on_enter_return_to_center(self):
+        # need to decide on code for saving position and calling it back
+        movement_msg = MovementCommand()
+        movement_msg.command = MovementCommand.MOVE_ABSOLUTE
+
+             # Create goal pose for position 0,0,0
+        goal_pose = GoalPose()
+        goal_pose.pose = Pose()
+        goal_pose.pose.position = Point(x=0.0, y=0.0, z=0.0)
+        goal_pose.pose.orientation = Quaternion(x=0.0, y=0.0, z=0.0, w=1.0)
+        goal_pose.copy_orientation = True
+
+        movement_msg.goal_pose = goal_pose
+        movement_msg.timeout_sec = 30.0
+        movement_msg.radius_of_acceptance = 0.5
+
+        # Send movement command
+        success = self.movement_client.send_movement_command(
+            movement_msg,
+            on_success=self.your_success_callback,
+            on_failure=self.abort,
+        )
+
+        if not success:
+            self.ros_node.get_logger().error("Failed to send move absolute command")
+            self.queued_method = self.abort
+
         
-    #     success = self.movement_client.send_movement_command(
-    #         movement_msg,
-    #         on_success=self.returning_to_center_done,
-    #         on_failure=self.abort,
-    #     )
+    
+    def on_enter_rotating_scan(self):
+        """Start the RotatingScanStateMachine as a sub-state."""
+        self.start_sub_state_machine(
+            RotatingScanStateMachine,
+            success_callback=self.rotating_scan_done,
+            fail_callback=self.abort,
+        )
+
+    def rotating_scan_done(self):
+        # Called when the rotating scan finishes successfully
+        self.ros_node.get_logger().info("Rotating scan completed.")
         
-    #     if not success:
-    #         self.ros_node.get_logger().error("Failed to send return to center command")
-    #         self.queued_method = self.abort
-            
