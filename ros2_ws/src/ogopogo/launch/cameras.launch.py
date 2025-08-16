53655bbf8bc69a5e2dfa9e8f9d961432800b6d48
ErykHalicki <erykhalicki0@gmail.com>
2025-08-16 18:48:12 +0000

    cameras

---
+++ ros2_ws/src/ogopogo/launch/cameras.launch.py
@@ -0,0 +1,41 @@
+import os
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
+
+def generate_launch_description():
+    realsense_launch_path = PathJoinSubstitution([
+        FindPackageShare('realsense2_camera'),
+        'launch',
+        'rs_launch.py'
+    ])
+
+    d455_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(realsense_launch_path),
+        launch_arguments={
+            'device_type': 'd455',
+            'camera_name': 'front',
+            'camera_namespace': 'ogopogo',
+            'log_level': 'ERROR',
+            'unite_imu_method': '2',
+            'enable_gyro': 'true',
+            'enable_accel': 'true'
+        }.items()
+    )
+
+    d405_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(realsense_launch_path),
+        launch_arguments={
+            'device_type': 'd405',
+            'camera_name': 'bottom',
+            'camera_namespace': 'ogopogo',
+            'log_level': 'ERROR'
+        }.items()
+    )
+
+    return LaunchDescription([
+        d455_launch,
+        d405_launch
+    ])
